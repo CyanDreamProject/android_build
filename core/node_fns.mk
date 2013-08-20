4a8788e9ab4785ac92fb49b41e6c8e841e97e6ab
Andrew Montgomery-Fox <andrewmf2011@gmail.com>
2013-08-20 11:31:13 +0100

    Revert "Update node_fns.mk"
    This reverts commit 0b563cb1579b793583fe09ca75d747b05a65278a.

--- core/node_fns.mk
+++ core/node_fns.mk
@@ -200,8 +200,8 @@ define _import-node
 
   $(call _expand-inherited-values,$(1),$(2),$(3))
 
-  $(eval $(1).$(2).inherited :=)
-  $(eval _include_stack := $(wordlist 2,9999,$$(_include_stack)))
+  $(eval $(1).$(2).inherited := 
+  $(eval _include_stack := $(wordlist 2,9999,$$(_include_stack))))
 endef
 
 #
